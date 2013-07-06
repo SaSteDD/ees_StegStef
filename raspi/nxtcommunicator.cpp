ae464df02c906e6abd2eef4c8ced50727f24963e
Sascha Steffen <steffensascha@gmail.com>
2013-07-06 23:14:34 +0200

    vl. klappt es so

--- raspi/nxtcommunicator.cpp
+++ raspi/nxtcommunicator.cpp
@@ -273,7 +273,7 @@ void NxtCommunicator::readStateData()
           state.stepNum.byte3=st[4];
 
       emit nxtStateChanged(state);
-          emit nxtLogMessage(QString(stateChar.first + "( " + stateChar.second + "): " + chToHexString(st[0])
+          emit nxtLogMessage(QString(stateChar.first + "(NXT-Status): " + chToHexString(st[0])
               + chToHexString(st[1])
               + chToHexString(st[2])
               + chToHexString(st[3])
